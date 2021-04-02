d50aed7d5aa5a8457fb37c4609e074f4079fdeb9
llohi <joose.lohi01@gmail.com>
2021-04-02 11:54:52 +0300

    edits

--- PROJECTS/gui_project/main.py
+++ PROJECTS/gui_project/main.py
@@ -25,6 +25,11 @@ class UserInterface:
         self.root.geometry('1030x720')
         self.root.title('Who Wants to Be a Millionaire?')
 
+        #  if run == False, break program
+        self.run = True
+        self.questions = []
+        self.question = ''
+
         #  set background
         background = Label(bg='#FFF', width=1280, height=720)
         background.place(x=0, y=0)
@@ -39,9 +44,11 @@ class UserInterface:
         #  frame for questions and answers:
         #   - START button
         #   - frame for question string
+        #   - frame for answers
         self.middle_frame = Frame(self.root, width=600, height=520, bg='#FFF', bd=5, relief=RIDGE)
         self.start_button = Button(self.middle_frame, text='START', bg='green', fg='white', padx=20, pady=20, bd=5, relief=RIDGE, command=self.ask_questions)
         self.question_frame = Frame(self.middle_frame, width=500, height=100, bg='#FFF')
+        self.answer_frame = Frame(self.middle_frame, width=536, height=265, bg='#FFF')
 
         #  frame for right sidebar:
         #   - prize count
@@ -49,7 +56,7 @@ class UserInterface:
         #   - QUIT button
         self.right_frame = Frame(self.root, width=100, height=520, bg='#FFF', bd=5, relief=RIDGE)
         self.count = 0
-        self.count_label = Label(self.right_frame, text=f'CORRECT:\n{self.count}', bg='#FFF')
+        self.count_label = Label(self.right_frame, text=f'PRIZE:\n0$', bg='#FFF')
         self.ff = Button(self.right_frame, text='50/50', padx=27, pady=15)
         self.ask = Button(self.right_frame, text='ASK THE\nAUDIENCE', pady=10)
 
@@ -82,8 +89,6 @@ class UserInterface:
         self.middle_frame.grid(row=1, column=1, padx=20, pady=20)
         self.middle_frame.grid_propagate(0)  # freeze size of frame
         self.start_button.grid(row=1, column=0, padx=250, pady=50)
-        self.question_frame.grid(row=0, column=0, padx=50, pady=20)
-        self.question_frame.grid_propagate(0)
 
         #  RIGHT
 
@@ -97,6 +102,7 @@ class UserInterface:
     #  initialize event loop
     def start(self):
 
+        self.questions = list(QA)
         self.root.mainloop()
 
     #  ask questions
@@ -106,55 +112,97 @@ class UserInterface:
         #  remove button
         self.start_button.grid_forget()
 
-        self.answer_frame = Frame(self.middle_frame, width=536, height=265, bg='#FFF')
+        #  grid question and answer frames
+        self.question_frame.grid(row=0, column=0, padx=50, pady=20)
+        self.question_frame.grid_propagate(0)
         self.answer_frame.grid(row=1, column=0, padx=23)
         self.answer_frame.grid_propagate(0)
 
-        #  list of all questions
-        questions = []
-        for q in QA:
-            questions.append(q)
+        # grid the question
+        self.question = random.choice(self.questions)
+        msg = Message(self.question_frame, text=self.question, width=490, bg='#FFF', anchor='center', justify='center', font=('Helvetica', 16))
+        msg.grid(row=0, column=0)
 
-        # ask 12 questions
-        x = 0
-        while x < 1:
+        # answers for the question
+        answers = QA[self.question]
 
-            question = random.choice(list(questions))
-            questions.remove(question)  # dont ask question again
+        # shuffle answers
+        items = list(answers.items())
+        random.shuffle(items)
+        answers = dict(items)
 
-            # answers for the question
-            answers = []
-            for answer in QA[question]:
-                answers.append(QA[question][answer])
-
-            random.shuffle(answers)
-
-            print(answers)
-
-            # grid the question
-            msg = Message(self.question_frame, text=question, width=490, bg='#FFF', anchor='center', justify='center')
-            msg.config(font=('Helvetica', 15))
-            msg.grid(row=0, column=0)
-
-            # grid the answers
-            Button(self.answer_frame, text=answers[0], width=30, height=7).grid(row=0, column=0)
-            Button(self.answer_frame, text=answers[1], width=30, height=7).grid(row=0, column=1)
-            Button(self.answer_frame, text=answers[2], width=30, height=7).grid(row=1, column=0)
-            Button(self.answer_frame, text=answers[3], width=30, height=7).grid(row=1, column=1)
-
-            x += 1
+        #  grid the answers:
+        #      A    B
+        #      C    D
+        Button(self.answer_frame, text=list(answers)[0], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[0]], msg)).grid(row=0, column=0)
+        Button(self.answer_frame, text=list(answers)[1], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[1]], msg)).grid(row=0, column=1)
+        Button(self.answer_frame, text=list(answers)[2], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[2]], msg)).grid(row=1, column=0)
+        Button(self.answer_frame, text=list(answers)[3], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[3]], msg)).grid(row=1, column=1)
 
 
-#  representation of a question
-class Question:
+    def click_answer(self, run, msg):
 
-    def __init__(self, question, correct, false_1, false_2, false_3):
+        #  if correct
+        if run == True:
 
-        self.question = question
-        self.correct = correct
-        self.false_1 = false_1
-        self.false_2 = false_2
-        self.false_3 = false_3
+            if self.count < 11:
+                msg.destroy()  # clear message from screen
+                self.questions.remove(self.question)  # make sure same question not asked again
+                self.count += 1
+                self.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.count - 1]]}$')  # udate prize text
+                self.ask_questions()  # go again
+
+            #  user answered all questions correctly
+            else:
+                #  clear the screen
+                msg.destroy()
+                self.question_frame.grid_forget()
+                self.answer_frame.grid_forget()
+
+                self.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.count - 1]]}$')
+
+                end_label = Label(self.middle_frame, text='Congratulations, you have won 1 000 000$!\nWant to play again?', background='#FFF', font=('Helvetica', 16))
+                end_label.grid(row=0, column=0, padx=80, pady=20)
+
+                restart_button = Button(self.middle_frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
+                restart_button.grid(row=1, column=0, pady=20)
+
+        #  if incorrect
+        else:
+
+            #  clear the screen
+            msg.destroy()
+            self.question_frame.grid_forget()
+            self.answer_frame.grid_forget()
+
+            if self.count > 0:
+                end_msg = f'Wrong answer, you won {PRIZES[list(PRIZES)[self.count - 1]]}$, good luck next time !'
+
+            else:
+                end_msg = 'Wrong answer. You won nothing, good luck next time!'
+
+            #  label ending message
+            end_label = Label(self.middle_frame, text=end_msg, background='#FFF', font=('Helvetica', 16))
+            end_label.grid(row=0, column=0, padx=40, pady=20)
+
+            #  label restart button
+            restart_button = Button(self.middle_frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
+            restart_button.grid(row=1, column=0, pady=20)
+
+
+    def restart(self, label, button):
+
+        #  clear screen
+        label.destroy()
+        button.destroy()
+
+        #  update all questions back to list and zero counter
+        self.questions = list(QA)
+        self.count = 0
+        self.count_label.config(text = f'PRIZE:\n0$')
+
+        #  restart game
+        self.ask_questions()
 
 
 #  shortcut to directory with files
@@ -162,23 +210,21 @@ class Question:
 #     program is not ran from the same directory as files
 path = __file__.strip(os.path.basename(__file__))
 
-
-# dict for prizes
+# dictionary for prizes
 PRIZES = {
     '1': '500', '2': '1 000', '3': '2 000', '4': '5 000',
     '5': '10 000', '6': '20 000', '7': '50 000', '8': '75 000',
     '9': '150 000', '10': '250 000', '11': '500 000', '12': '1 000 000'
     }
 
-# dict for q&a
+#  create dictionary for questions and answers
+#  {'question_1?': {'answer_1': True, 'answer_2': False, 'answer_3': False, 'answer_4': False}}
 QA = {}
 file = open(f'{path}/questions.csv')
 
 for row in file:
     row = row.rstrip().split(';')
-
-    q = Question(row[0], row[1], row[2], row[3], row[4])
-    QA[row[0]] = {'true': q.correct, 'false_1': q.false_1, 'false_2': q.false_2, 'false_3': q.false_3}
+    QA[row[0]] = {row[1]: True, row[2]: False, row[3]: False, row[4]: False}
 
 def main():
 

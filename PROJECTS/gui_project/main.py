8b600a61e322779793e0406daf9e8ecbd4b60518
llohi <joose.lohi01@gmail.com>
2021-04-03 13:00:08 +0300

    EDITS TO MAIN.PY

--- PROJECTS/gui_project/main.py
+++ PROJECTS/gui_project/main.py
@@ -5,10 +5,18 @@ Opiskelijanumero: 050800360
 
 This program simulates the game show known as Who Wants to Be a Millionaire.
 The player is given a question and four options. If the answer is correct,
-the prize increases and the player moves on to the next question. If the answer is incorrect,
-the game ends. To win, the player must answer all 12 questions correctly.
-"""
+the prize increases and the player moves on to the next question. If the answer is
+incorrect, the game ends. To win, the player must answer all 12 questions correctly.
+The player is also given one skip.
 
+The program is structured in the following way:
+
+    parent: UserInterface
+  children: LeftFrame, MiddleFrame and RightFrame
+
+Each child class has its own attributes and will be called
+in class UserInterface. All action is initialized in main().
+"""
 
 import os
 import random
@@ -22,11 +30,16 @@ RULES = """
     Welcome to the game!
 
     The rules are simple:
+
         - choose the correct answer and your prize gets bigger
+
+        - you have one skip
+
         - answer all 12 questions correctly to win 1 000 000$
 
     Good Luck!
     """
+
 #  dictionary for prizes
 PRIZES = {
     '1': '500', '2': '1 000', '3': '2 000', '4': '5 000',
@@ -44,45 +57,89 @@ for row in file:
     QA[row[0]] = {row[1]: True, row[2]: False, row[3]: False, row[4]: False}
 
 
-#  representation of the user interface
+#  chart with prize info
+class LeftFrame:
+
+    #  'root' variable is the main window
+    def __init__(self, root):
+
+        #  create
+        self.frame = Frame(root, width=175, height=520, bg='#FFF', bd=5, relief=RIDGE)
+
+        # question/prize
+        Label(self.frame, text='QUESTION', bg='#FFF').grid(row=0, column=0, padx=3, pady=10)
+        Label(self.frame, text='PRIZE', bg='#FFF').grid(row=0, column=1, padx=3, pady=10)
+
+        # label prizes
+        i = 1
+        for num in PRIZES:
+            Label(self.frame, text=num, bg='#FFF').grid(row=i, column=0, pady=9)
+            Label(self.frame, text=f'{PRIZES[num]} $', bg='#FFF').grid(row=i, column=1)
+            i += 1
+
+
+#  1) print rules and display start button
+#  2) show question and answers
+class MiddleFrame:
+
+    def __init__(self, root, start_function):
+
+        #  create
+        self.frame = Frame(root, width=600, height=520, bg='#FFF', bd=5, relief=RIDGE)
+        self.rules = Message(self.frame, text=RULES, font=('Helvetica', 13), bg='#FFF', width=560, padx=20, pady=20)
+        self.start_button = Button(self.frame, text='START', bg='green', fg='white', padx=20, pady=20, bd=5, relief=RIDGE, command=start_function)
+        self.question_frame = Frame(self.frame, width=500, height=100, bg='#FFF')
+        self.answer_frame = Frame(self.frame, width=536, height=265, bg='#FFF')
+
+        #  grid
+        self.rules.grid(row=0, column=0)
+        self.start_button.grid(row=1, column=0, padx=230, pady=10)
+
+
+#  show three things: prize count, SKIP button and QUIT button
+class RightFrame:
+
+    def __init__(self, root, skip_function):
+
+        #  create
+        self.frame = Frame(root, width=100, height=520, bg='#FFF', bd=5, relief=RIDGE)
+        self.count = 0
+        self.count_label = Label(self.frame, text=f'PRIZE:\n0$', bg='#FFF')
+        self.skip = Button(self.frame, text='SKIP', bg='#0039E6', fg='#FFF', padx=25, pady=15, bd=5, relief=RIDGE, state=DISABLED, command=skip_function)
+        self.quit = Button(self.frame, text='QUIT', bg='red', fg='white', padx=26, pady=15, bd=5, relief=RIDGE, command=root.quit)
+
+        #  grid
+        self.count_label.grid(row=0, column=0, pady=10)
+        self.skip.grid(row=1, column=0, pady=10)
+        self.quit.grid(row=2, column=0, pady=10)
+
 class UserInterface:
 
-    #  initialize all frames and variables of the user interface here
     def __init__(self):
 
-        #  create root window with size and title
+        #  create main window with resolution 1010x640, title and white background
         self.root = Tk()
         self.root.geometry('1010x640')
         self.root.title('Who Wants to Be a Millionaire?')
-        #  set background
-        background = Label(bg='#FFF', width=1280, height=720)
-        background.place(x=0, y=0)
+        background = Label(bg='#FFF', width = 1010, height=640).place(x=0, y=0)
 
-        #  initialize variables
+        #  initialize variables used to store questions
         self.questions = []
         self.question = ''
 
-        #  set header
-        self.header = Label(text='Who Wants to Be a Millionaire?', bg='#FFF')
-        self.header.config(font=('Serif', 30))
+        #  header displays the title
+        self.header = Label(text='Who Wants to Be a Millionaire?', bg='#FFF', font=('Serif', 30))
 
-        #  set left frame
-        self.left_frame = Frame(self.root, width=175, height=520, bg='#FFF', bd=5, relief=RIDGE)
+        #  left frame
+        self.left = LeftFrame(self.root)
 
-        #  set middle frame:  rules, START button, frame for question and frame for answers
-        self.middle_frame = Frame(self.root, width=600, height=520, bg='#FFF', bd=5, relief=RIDGE)
-        self.rules = Message(self.middle_frame, text=RULES, font=('Helvetica', 15), bg='#FFF', width=560, padx=20, pady=20)
-        self.start_button = Button(self.middle_frame, text='START', bg='green', fg='white', padx=20, pady=20, bd=5, relief=RIDGE, command=self.start_with_skip)
-        self.question_frame = Frame(self.middle_frame, width=500, height=100, bg='#FFF')
-        self.answer_frame = Frame(self.middle_frame, width=536, height=265, bg='#FFF')
+        #  middle frame
+        self.middle = MiddleFrame(self.root, self.start_with_skip)
 
-        #  set right frame:  prize count, SKIP button and QUIT button
-        self.right_frame = Frame(self.root, width=100, height=520, bg='#FFF', bd=5, relief=RIDGE)
-        self.count = 0
-        self.count_label = Label(self.right_frame, text=f'PRIZE:\n0$', bg='#FFF')
-        self.skip = Button(self.right_frame, text='SKIP', bg='#0039E6', fg='#FFF', padx=25, pady=15, bd=5, relief=RIDGE, state=DISABLED, command=self.skip_question)
-        self.quit = Button(self.right_frame, text='QUIT', bg='red', fg='white', padx=26, pady=15, bd=5, relief=RIDGE, command=self.root.quit)
+        #  right frame
+        self.right = RightFrame(self.root, self.skip_question)
 
+    #  generate question list
     #  initialize event loop
     def start(self):
 
@@ -90,60 +147,47 @@ class UserInterface:
         self.grid_ui()
         self.root.mainloop()
 
-    #  initialize grid system
+
+    #  grid all class attributes
+    #  notice many attributes have been pre-gridded in child classes
     def grid_ui(self):
-        #                       HEADER
-        #  LEFT FRAME        MIDDLE FRAME        RIGHT FRAME
 
         self.header.grid(row=0, column=0, columnspan=3, pady=10)
 
-        self.left_frame.grid(row=1, column=0, padx=20, pady=20)
-        self.left_frame.grid_propagate(0)  # freeze size of frame
-        # question/prize
-        Label(self.left_frame, text='QUESTION', bg='#FFF').grid(row=0, column=0, padx=3, pady=10)
-        Label(self.left_frame, text='PRIZE', bg='#FFF').grid(row=0, column=1, padx=3, pady=10)
-        # label prizes
-        i = 1
-        for num in PRIZES:
-            Label(self.left_frame, text=num, bg='#FFF').grid(row=i, column=0, pady=9)
-            Label(self.left_frame, text=f'{PRIZES[num]} $', bg='#FFF').grid(row=i, column=1)
-            i += 1
+        self.left.frame.grid(row=1, column=0, padx=20, pady=20)
+        self.left.frame.grid_propagate(0)  # freeze size of frame
 
-        self.middle_frame.grid(row=1, column=1, padx=20, pady=20)
-        self.middle_frame.grid_propagate(0)  # freeze size of frame
-        self.rules.grid(row=0, column=0)
-        self.start_button.grid(row=1, column=0, padx=250, pady=20)
+        self.middle.frame.grid(row=1, column=1, padx=20, pady=20)
+        self.middle.frame.grid_propagate(0)
 
-        self.right_frame.grid(row=1, column=2, padx=20, pady=100)
-        self.count_label.grid(row=0, column=0, pady=10)
-        self.skip.grid(row=1, column=0, pady=10)
-        self.quit.grid(row=2, column=0, pady=10)
+        self.right.frame.grid(row=1, column=2, padx=20, pady=100)
+
 
     #  the purpose of this function is to start the
     #  game with the skip button enabled
     def start_with_skip(self):
 
         #  enable skip button
-        self.skip.config(state = NORMAL)
+        self.right.skip.config(state = NORMAL)
         self.ask_questions()
 
 
-    #  ask question and create interactive buttons for answers
+    #  ask questions and create interactive buttons for answers
     def ask_questions(self):
 
         #  clear screen
-        self.rules.grid_forget()
-        self.start_button.grid_forget()
+        self.middle.rules.grid_forget()
+        self.middle.start_button.grid_forget()
 
         #  grid frames and freeze their size
-        self.question_frame.grid(row=0, column=0, padx=50, pady=20)
-        self.answer_frame.grid(row=1, column=0, padx=23)
-        self.question_frame.grid_propagate(0)
-        self.answer_frame.grid_propagate(0)
+        self.middle.question_frame.grid(row=0, column=0, padx=50, pady=20)
+        self.middle.answer_frame.grid(row=1, column=0, padx=23)
+        self.middle.question_frame.grid_propagate(0)
+        self.middle.answer_frame.grid_propagate(0)
 
         #  grid the question inside its frame
         self.question = random.choice(self.questions)
-        q = Message(self.question_frame, text=self.question, width=490, bg='#FFF', anchor='center', justify='center', font=('Helvetica', 16))
+        q = Message(self.middle.question_frame, text=self.question, width=490, bg='#FFF', anchor='center', justify='center', font=('Helvetica', 16))
         q.grid(row=0, column=0)
 
         #  get answers from global dictionary QA
@@ -157,10 +201,10 @@ class UserInterface:
         #  grid the answers:
         #      A    B
         #      C    D
-        Button(self.answer_frame, text=list(answers)[0], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[0]], q)).grid(row=0, column=0)
-        Button(self.answer_frame, text=list(answers)[1], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[1]], q)).grid(row=0, column=1)
-        Button(self.answer_frame, text=list(answers)[2], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[2]], q)).grid(row=1, column=0)
-        Button(self.answer_frame, text=list(answers)[3], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[3]], q)).grid(row=1, column=1)
+        Button(self.middle.answer_frame, text=list(answers)[0], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[0]], q)).grid(row=0, column=0)
+        Button(self.middle.answer_frame, text=list(answers)[1], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[1]], q)).grid(row=0, column=1)
+        Button(self.middle.answer_frame, text=list(answers)[2], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[2]], q)).grid(row=1, column=0)
+        Button(self.middle.answer_frame, text=list(answers)[3], width=30, height=7, command=lambda: self.click_answer(answers[list(answers)[3]], q)).grid(row=1, column=1)
 
 
     #  react to the answer button according to
@@ -171,29 +215,29 @@ class UserInterface:
         if run == True:
 
             #  if not last question
-            if self.count < 11:
+            if self.right.count < 11:
                 q.destroy()  # clear message from screen
                 self.questions.remove(self.question)  # make sure same question not asked again
-                self.count += 1
-                self.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.count - 1]]}$')  # udate prize text
+                self.right.count += 1
+                self.right.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.right.count - 1]]}$')  # udate prize text
                 self.ask_questions()  # go again
 
             #  user answered all questions correctly
             else:
                 #  clear the screen
                 q.destroy()
-                self.question_frame.grid_forget()
-                self.answer_frame.grid_forget()
+                self.middle.question_frame.grid_forget()
+                self.middle.answer_frame.grid_forget()
 
                 #  disable skip button
-                self.skip.config(state = DISABLED)
+                self.right.skip.config(state = DISABLED)
 
-                self.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.count - 1]]}$')
+                self.right.count_label.config(text = f'PRIZE:\n{PRIZES[list(PRIZES)[self.right.count - 1]]}$')
 
-                end_label = Label(self.middle_frame, text='Congratulations, you have won 1 000 000$!\nWant to play again?', bg='#FFF', font=('Helvetica', 16))
+                end_label = Label(self.middle.frame, text='Congratulations, you have won 1 000 000$!\nWant to play again?', bg='#FFF', font=('Helvetica', 16))
                 end_label.grid(row=0, column=0, padx=80, pady=20)
 
-                restart_button = Button(self.middle_frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
+                restart_button = Button(self.middle.frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
                 restart_button.grid(row=1, column=0, pady=20)
 
         #  if incorrect
@@ -201,37 +245,38 @@ class UserInterface:
 
             #  clear the screen
             q.destroy()
-            self.question_frame.grid_forget()
-            self.answer_frame.grid_forget()
+            self.middle.question_frame.grid_forget()
+            self.middle.answer_frame.grid_forget()
 
             #  disable skip button
-            self.skip.config(state = DISABLED)
+            self.right.skip.config(state = DISABLED)
 
-            if self.count > 0:  # player got atleast one correct answer
-                end_msg = f'Wrong answer, you won {PRIZES[list(PRIZES)[self.count - 1]]}$, good luck next time !'
+            if self.right.count > 0:  # player got atleast one correct answer
+                end_msg = f'Wrong answer, you won {PRIZES[list(PRIZES)[self.right.count - 1]]}$, good luck next time !'
 
             else:
                 end_msg = 'Wrong answer. You won nothing, good luck next time!'
 
             #  grid ending message
-            end_label = Label(self.middle_frame, text=end_msg, bg='#FFF', font=('Helvetica', 16))
+            end_label = Label(self.middle.frame, text=end_msg, bg='#FFF', font=('Helvetica', 16))
             end_label.grid(row=0, column=0, padx=40, pady=20)
 
             #  grid restart button
-            restart_button = Button(self.middle_frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
+            restart_button = Button(self.middle.frame, text='RESTART', command=lambda: self.restart(end_label, restart_button))
             restart_button.grid(row=1, column=0, pady=20)
+
 
     #  skip current question and move to next one
     def skip_question(self):
 
         #  disable the skip button
-        self.skip.config(state = DISABLED)
+        self.right.skip.config(state = DISABLED)
 
         #  clear frames
-        for widget in self.question_frame.winfo_children():
+        for widget in self.middle.question_frame.winfo_children():
             widget.grid_forget()
 
-        for widget in self.answer_frame.winfo_children():
+        for widget in self.middle.answer_frame.winfo_children():
             widget.grid_forget()
 
         #  remove question from list
@@ -241,22 +286,22 @@ class UserInterface:
         self.ask_questions()
 
 
-    #  function initialized if player wants to go again
-    #  clear screen  ->  update class attributes  ->  start asking questions
+    #  clear screen
+    #  update class attributes
+    #  restart
     def restart(self, label, button):
 
-        #  clear screen
+        #  clear
         label.destroy()
         button.destroy()
 
-        #  update all questions back to list and zero counter
+        #  update
         self.questions = list(QA)
-        self.count = 0
-        self.count_label.config(text = f'PRIZE:\n0$')
+        self.right.count = 0
+        self.right.count_label.config(text = f'PRIZE:\n0$')
 
-        #  restart game with one skip
+        #  restart
         self.start_with_skip()
-
 
 def main():
 
